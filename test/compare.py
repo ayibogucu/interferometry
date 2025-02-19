0fbcdc52ef7c2f30098b7597e85f48276bce2ca5
ayibogucu <ismail.inal999@gmail.com>
2025-02-19 22:08:08 +0300

    visualization changes

--- test/compare.py
+++ test/compare.py
@@ -1,8 +1,8 @@
 import numpy as np
 from scipy.io import loadmat
 from cv2 import minMaxLoc
-import lib.plot as plt
-# import matplotlib.pyplot as plt
+import lib.plot
+import matplotlib.pyplot as plt
 
 python_array = np.load("./results/100.npy")
 matlab_array = -loadmat("./results/unwrapped_phase.mat")["s"]
@@ -17,25 +17,25 @@ print(f"MSE: {mse},  PSNR: {psnr}")
 print(f"minimum difference is {min_val} at location {min_loc}")
 print(f"maximum difference is {max_val} at location {max_loc}")
 
-plt.plot_mesh(difference)
 
+fig, ax = plt.subplots(2, 2, figsize=(15, 15))
 
-# fig, ax = plt.subplots(2, 2, figsize=(15, 15))
-#
-# ax[0, 0].imshow(python_array, cmap="jet")
-# ax[0, 0].set_title("Python Image")
-# ax[0, 0].axis("off")
-#
-# ax[0, 1].imshow(matlab_array, cmap="jet")
-# ax[0, 1].set_title("Matlab Image")
-# ax[0, 1].axis("off")
-#
-# cax = ax[1, 0].imshow(difference, cmap="jet", vmin=0, vmax=np.max(difference))
-# ax[1, 0].set_title("Difference Absolute")
-# ax[1, 0].axis("off")
-#
-# fig.colorbar(cax, ax=ax[1, 0], orientation="vertical", fraction=0.02, pad=0.04)
-#
-# ax[1, 1].axis("off")
-#
-# plt.show()
+ax[0, 0].imshow(python_array, cmap="jet")
+ax[0, 0].set_title("Python Image")
+ax[0, 0].axis("off")
+
+ax[0, 1].imshow(matlab_array, cmap="jet")
+ax[0, 1].set_title("Matlab Image")
+ax[0, 1].axis("off")
+
+cax = ax[1, 0].imshow(difference, cmap="jet", vmin=0, vmax=np.max(difference))
+ax[1, 0].set_title("Difference Absolute")
+ax[1, 0].axis("off")
+
+fig.colorbar(cax, ax=ax[1, 0], orientation="vertical", fraction=0.02, pad=0.04)
+
+ax[1, 1].axis("off")
+
+plt.show()
+
+lib.plot.plot_mesh(difference)
