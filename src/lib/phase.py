4875fab69862c1616ca13e3f240428c420195dff
ayibogucu <ismail.inal999@gmail.com>
2025-03-21 13:52:44 +0300

    finishing touches

--- src/lib/phase.py
+++ src/lib/phase.py
@@ -4,38 +4,23 @@ from skimage.restoration import unwrap_phase
 
 def fft(img: np.ndarray, rad: int, wavelength: float) -> np.ndarray:
     fft_image = np.fft.fft2(img)
-
     height, width = fft_image.shape
     cy, cx = height // 2, width // 2
 
-    # Define ROI boundaries
-    x_min, x_max = 0, cx
-    y_min, y_max = cy, height
+    roi = np.abs(fft_image[cy:height, :cx])
+    local_max_y, global_max_x = np.unravel_index(np.argmax(roi), roi.shape)
+    global_max_y = cy + local_max_y
 
-    # Crop the magnitude image to the ROI
-    roi = np.abs(fft_image[y_min:y_max, x_min:x_max])
-
-    # Find the coordinates of the maximum value in the ROI
-    local_max_y, local_max_x = np.unravel_index(np.argmax(roi), roi.shape)
-
-    # Map back to the full image coordinates
-    global_max_y = y_min + local_max_y
-    global_max_x = x_min + local_max_x
-
-    # Create a mask with a circle at the detected maximum
     y, x = np.ogrid[:height, :width]
     dist_sq = (y - global_max_y) ** 2 + (x - global_max_x) ** 2
     fft_image[dist_sq > rad**2] = 0
 
-    # Shift the FFT to center the sideband
     fft_centered_image = np.roll(
         np.roll(fft_image, -global_max_y, axis=0), -global_max_x, axis=1
     )
 
-    # Perform inverse FFT
     ifft_centered_image = np.fft.ifft2(fft_centered_image)
 
-    # Calculate the phase and unwrap it
     phase_matrix = np.angle(ifft_centered_image)
     phase_matrix_unwrapped = unwrap_phase(phase_matrix)
     return phase_matrix_unwrapped * wavelength * 0.15915494309189535
